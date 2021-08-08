8d881f49e7507e426044c1ad9f20f4f8b12d1f69
ptkeb <87351037+ptkeb@users.noreply.github.com>
2021-08-08 23:47:02 +0900

    Update algotype1-1(3).py

--- 1. 스택 & 큐/algotype1-1(3).py
+++ 1. 스택 & 큐/algotype1-1(3).py
@@ -2,6 +2,7 @@
 class aa:
     def solution(prices):
         answer = []
+        
         for i in range(len(prices)):
             count = 0
             pr = prices.pop(0)
@@ -9,23 +10,23 @@ class aa:
 
             for m in range(len(prices)):
                 if m == len(prices)-1:
-                    count = count + 1
+                    count += 1
                     answer.append(count)
                     print(a,i,m,"펑")
                     print(prices)
                     break
 
                 elif a <= prices[m]:
-                    count = count+1
+                    count += 1
                     print(a,i,m,"추가")
                     print(prices)
+                    
                 else:
-                    count = count + 1
+                    count += 1
                     answer.append(count)
                     print(a,i,m,"하락")
                     print(prices)
                     break
-
 
         answer.append(0)
         return answer
